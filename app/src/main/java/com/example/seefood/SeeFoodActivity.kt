a1c63fffa7b48d6caf540bfdfa03a4a1bdb0ee4e
arcanegolem <dopeflipper@gmail.com>
2023-03-30 23:40:13 +0300

    Применил плагин Hilt, добавил необходимые хилту аннотации и файлы, подогнал манифест под него

--- app/src/main/java/com/example/seefood/SeeFoodActivity.kt
+++ app/src/main/java/com/example/seefood/SeeFoodActivity.kt
@@ -3,7 +3,9 @@ package com.example.seefood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SeeFoodActivity : ComponentActivity()
 {
    override fun onCreate(savedInstanceState: Bundle?)
