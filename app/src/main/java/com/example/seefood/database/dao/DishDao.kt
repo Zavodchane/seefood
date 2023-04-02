179e1fbbd61723e9da4567c02939b6d7fdc15903
arcanegolem <dopeflipper@gmail.com>
2023-04-03 01:31:33 +0300

    Добавил зависимости для ROOM, вроде как сделал локальную бд и доступ к ней

---
+++ app/src/main/java/com/example/seefood/database/dao/DishDao.kt
@@ -0,0 +1,24 @@
+package com.example.seefood.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.seefood.database.objects.Dish
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DishDao {
+
+   @Upsert
+   suspend fun upsertDish(dish: Dish)
+
+   @Delete
+   suspend fun deleteDish(dish: Dish)
+
+   @Query("SELECT * FROM dishes WHERE is_favorite = TRUE")
+   suspend fun getFavoriteDishes() : Flow<List<Dish>>
+
+   @Query("SELECT * FROM dishes WHERE category = :category")
+   suspend fun getDishesByCategory(category: String) : Flow<List<Dish>>
+}
---
+++ app/src/main/java/com/example/seefood/database/objects/Dish.kt
@@ -0,0 +1,17 @@
+package com.example.seefood.database.objects
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dishes")
+data class Dish (
+   @ColumnInfo(name = "name")           val name         : String,
+   @ColumnInfo(name = "recipe")         val recipe       : String,
+   @ColumnInfo(name = "img_local_path") val imgLocalPath : String,
+   @ColumnInfo(name = "category")       val category     : String,
+   @ColumnInfo(name = "is_favorite")    val isFavorite   : Boolean,
+
+   @PrimaryKey (autoGenerate = true)
+   val id           : Int
+)
